ded238e7edd08ac9884fc99fb9f6a25b6bda8527
DiegoS284 <diego64g284@gmail.com>
2026-07-28 13:49:18 -0500

    test(scaffold): verify spring boot baseline

---
+++ src/test/java/com/nexa/api/NexaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
